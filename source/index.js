ed6ca2b061c9d3e0533eeaeb72ace99870dfe35d
Connor Winiarczyk <connorwiniarczyk@gmail.com>
2019-01-25 20:20:07 -0500

    quick update for getting POST data from groupme

--- source/index.js
+++ source/index.js
@@ -1,4 +1,2 @@
 const server = require('./server.js')
 const bunyan = require('bunyan')
-const program = require('commander')
-
--- source/server.js
+++ source/server.js
@@ -2,11 +2,26 @@ const express = require('express')
 const app = express()
 const http = require("http").Server(app)
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 const events = require('events')
 exports.events = new events()
 
 app.post('/groupme', function(req, res){
-	console.log(groupme)
+	console.log(req.body)
 })
 
 app.listen(4002)
+
+// quick test
+const fetch = require('node-fetch')
+fetch('http://localhost:4002/groupme', {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: "POST",
+    body: JSON.stringify({a: 1, b: 2})
+})
